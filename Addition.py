55611e68fd375cdc9534dc8f192f209c76074d97
sa3047 <swapnil_aher@yahoo.co.in>
2016-07-03 10:35:44 -0400

    2nd program addded

---
+++ Addition.py
@@ -0,0 +1,8 @@
+def main():
+	print "Addition program for 2 intergers."
+	firstNum = int(raw_input("Enter first number"))
+	secondNum = int(raw_input("Enter second number"))
+	sum = firstNum + secondNum
+	print "Addtion of 2 numbers :", sum
+	
+main()
